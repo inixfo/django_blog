cf1dbfc098d3481996e9078f83608a24cf736a9d
MD Abdul Ahad <iamahad1570@gmail.com>
2023-10-07 14:25:18 +0600

    4 september class

--- app_login/urls.py
+++ app_login/urls.py
@@ -4,5 +4,7 @@ from app_login import views
 app_name = 'app_login'
 
 urlpatterns = [
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- app_login/views.py
+++ app_login/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,5 +19,24 @@ def signup(request):
     diction = {'form': form, 'registered':registered}
 
     return render(request, 'app_login/signup.html', context=diction)
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+    return render (request, 'app_login/login.html', context={'form':form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
 
 
